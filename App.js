15b19c7be7bdd43cc66dd017a9059391026ee7e7
Marvin Ho <myuanHo1@gmail.com>
2020-07-27 20:42:35 -0700

    Create modal toggle for edit and delete

--- App.js
+++ App.js
@@ -13,36 +13,46 @@ import {
   StyleSheet,
   FlatList,
   ScrollView,
+  TouchableOpacity,
+  Modal,
   View,
   Text,
   StatusBar,
 } from 'react-native';
+// import console = require('console');
 
 // import console = require('console');
 
 const App: () => React$Node = () => {
-  const [todo, setTodoItems] = useState([
-    { item: 'Hello', key: '1'},
-    { item: 'example', key: '2'},
-    { item: 'data', key: '3'}
-  ]);
+  const [todo, setTodoItems] = useState(
+    [
+      {item: 'Hello', key: '1'},
+      {item: 'example', key: '2'},
+      {item: 'data', key: '3'},
+    ],
+    true,
+  );
+
+  const [toggled, setToggled] = useState(false);
 
   const newKey = (num) => {
-    console.log(typeof num);
     let keyNum = Number.parseInt(num, 10);
     let addKeyNum = keyNum + 1;
     let result = addKeyNum.toString();
     return result;
-  }
+  };
 
   const addTodoItems = (item) => {
-    let keyValue = `${parseInt(todo[todo.length - 1].key)}`;
+    let keyValue = `${todo.length}`;
     setTodoItems((todo) => {
       return [{item: item, key: newKey(keyValue)}, ...todo];
     });
   };
 
-  {console.log(todo)}
+  const toggleModal = () => {
+    setToggled((toggled) => !toggled);
+  }
+
   return (
     <SafeAreaView>
       <View>
@@ -51,15 +61,37 @@ const App: () => React$Node = () => {
           data={todo}
           renderItem={(allTodo) => {
             let allItems = allTodo.item.item;
-            return <Text>{allItems}</Text>;
+            return (
+              <TouchableOpacity onPress={toggleModal}>
+                <Text>{allItems}</Text>
+              </TouchableOpacity>
+            );
           }}
         />
       </View>
+      <Modal transparent={true} visible={toggled}>
+        <View style={styles.outerModal}>
+          <View style={styles.modal}>
+            <Text>Modal</Text>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  outerModal: {
+    backgroundColor: 'transparent',
+    flex: 1,
+  },
+  modal: {
+    backgroundColor: 'cyan',
+    margin: 50,
+    padding: 40,
+    borderRadius: 10,
+    flex: 1,
+  },
 });
 
 export default App;
